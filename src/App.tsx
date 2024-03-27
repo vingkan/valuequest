c41090abd1836190aa326f51728b69691fd91174
Vinesh Kannan <vingkan@gmail.com>
2024-03-27 19:59:23 +0000

    build: Set up Vite, GitPod, and GitHub Pages.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+    return (
+        <div>
+            <h1>Value Quest</h1>
+            <p>A game about value-based care.</p>
+        </div>
+    )
+}
---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+/* global document */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './styles/Reset.css'
+import './styles/Main.css'
+
+// Set page title for entire app
+document.title = 'Value Quest'
+
+// Initialize React app
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <BrowserRouter basename="valuequest">
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+)
