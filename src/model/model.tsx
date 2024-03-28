bed8d64f7d2cb239f43706da005156950838962e
Vinesh Kannan <vingkan@gmail.com>
2024-03-28 00:52:57 +0000

    docs: Choose metrics for quintuple aim.

---
+++ src/model/model.tsx
@@ -0,0 +1,7 @@
+/*
+ * Member Experience = Member Satisfaction Score (0-1)
+ * Health Outcomes = Quality of Life Score (0-1)
+ * Cost of Healthcare = Average Cost Per Member Per Month ($)
+ * Provider Experience = Provider Satisfaction Score (0-1)
+ * Health Equity = Gini Index of Quality of Life Score (0-1)
+ */
